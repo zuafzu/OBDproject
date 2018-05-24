7d7b0f61df0cd7abe3b90d66f33a5cef85c5bd6a
wangyuting@huihaiedu.net <231147wangyuting>
2018-05-24 08:51:31 +0800

    刷写worker

--- app/src/main/java/com/cy/obdproject/worker/WriteDataWorker.java
+++ app/src/main/java/com/cy/obdproject/worker/WriteDataWorker.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Handler;
 import android.util.Log;
 
+import com.cy.obdproject.agreement.ECUagreement;
 import com.cy.obdproject.bean.WriteFileBean;
 import com.cy.obdproject.callback.SocketCallBack;
 import com.cy.obdproject.socket.MySocketClient;
@@ -155,7 +156,67 @@ public class WriteDataWorker {
 //                    return;
 //                }
 //                putData("刷写成功");
-            }
+                // 1、发送7DF  指令1003 长度2 不需要接收
+                msg = ECUagreement.a("1003");
+                // 2、发送7DF  指令 长度2  只发送不需要接收
+                msg = ECUagreement.a("3E80");
+                // 3、发送7DF  指令8502 长度2  只发送不需要接收
+                msg = ECUagreement.a("8502");
+                // 4、发送7DF  指令 长度3  只发送不需要接收
+                msg = ECUagreement.a("280303");
+                // 5、发送7A2 指令1002 长度2 返回 7AA 5002
+                msg = ECUagreement.a("1002");
+                if (replay()){
+                    return;
+                }
+                //6、发送7A2 指令2703   长度2  返回  7AA 6703   四个字节seed（随机产生，给算法，经运算后下一步发送）
+                msg = ECUagreement.a("2703");
+                if (replay()){
+                    return;
+                }
+                //7、发送7A2 指令2704+seed运算后的数值   长度6   返回 7AA 6704
+                msg = ECUagreement.a("2704" +"seed的运算数值");
+                if (replay()){
+                    return;
+                }
+                //8、CANID 7A2   指令 2EF19831323334353637383900     长度 13    返回 7AA  6EF198
+                msg = ECUagreement.a("2EF19831323334353637383900");
+                if (replay()){
+                    return;
+                }
+                //9、CANID 7A2   指令2EF199   长度 7 返回 7AA  6EF199
+                msg = ECUagreement.a("2EF199");
+                if (replay()){
+                    return;
+                }
+                //10、CANID 7A2   指令340044 00000000(EOl中的addr) 000003B2(eol中的len)   长度 11 返回 7AA  74...
+                msg = ECUagreement.a("34004400000000000003B2");
+                if (replay()){
+                    return;
+                }
+                //11、CANID 7A2  指令 3601 + eol中的数据（从00开始到946字节） 长度948  返回 7AA 7601
+                msg = ECUagreement.a("3601" +" eol中的数据（从00开始到946字节）");
+                if (replay()){
+                    return;
+                }
+                //12、CANID 7A2 指令 37  长度1 返回 77
+                msg = ECUagreement.a("37");
+                if (replay()){
+                    return;
+                }
+                //13、CANID 7A2 指令 310102026E2ECDCD（一次性校验）  长度8 返回 71
+                msg = ECUagreement.a("310102026E2ECDCD");
+                if (replay()){
+                    return;
+                }
+                //14、CANID 7A2 指令 3101FF0044 00FE0000(addr) 0009F230(len)(这条指令是数据擦除) 返回7AA 7101FF
+                msg = ECUagreement.a("3101FF004400FE00000009F230");
+                if (replay()){
+                    return;
+                }
+                //15、CANID 7A2 指令
+
+         }
         }).start();
     }
 
